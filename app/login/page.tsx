35bc485f06466aac01c477ad816173fec63ada36
unknown <skaty314@gmail.con>
2026-07-12 19:03:21 -0600

    Primer Commit

---
+++ app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+    return (
+        <main style={{ padding: "2rem "}}>
+          <h1>Inicia Sesion</h1>
+          <p>Pantalla de acceso para usuario registrado.</p>
+        </main>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Gestor de Solicitudes</h1>
+      <p>Plataforma web para la gestión de solicitudes de soporte, permisos y requerimientos.</p>
+    </main>
+  );
+}
